6d36e7fc983b6523ddf3430fe2014a3e7a915c99
roisu <roisusi@gmail.com>
2021-07-21 14:47:35 +0300

    Ex01
    2-3 completed

---
+++ C21 Ex01 RoiSusi 300685906 Izabela Blitsman 308278514/d/C21_Ex01_2/BegginersSandMachine.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Text;
+
+namespace C21_Ex01_2
+{
+    public class BeginnersSandMachine
+    {
+
+        public static StringBuilder CreateSandMachine(StringBuilder i_SandMachineStringBuilder , int i_SandMachineRows , int i_SandMachineHeight)
+        {
+            if (i_SandMachineRows == i_SandMachineHeight)
+                return i_SandMachineStringBuilder;
+
+            if (i_SandMachineRows < i_SandMachineHeight / 2)
+            {
+                i_SandMachineStringBuilder.AppendLine(new string(' ', i_SandMachineRows) + new string('*', i_SandMachineHeight - (2 * i_SandMachineRows)));
+            }
+            else
+            {
+                i_SandMachineStringBuilder.AppendLine(new string(' ', i_SandMachineHeight - i_SandMachineRows - 1) + new string('*', (2 * i_SandMachineRows) - i_SandMachineHeight + 2));
+            }
+
+            CreateSandMachine(i_SandMachineStringBuilder, i_SandMachineRows + 1, i_SandMachineHeight);
+
+            return i_SandMachineStringBuilder;
+
+        }
+
+        public static void PrintSandMachine(StringBuilder i_stringBuilder)
+        {
+            Console.WriteLine(i_stringBuilder);
+        }
+
+
+        public static void Main()
+        {
+            PrintSandMachine(CreateSandMachine(new StringBuilder(), 0, 4));
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ C21 Ex01 RoiSusi 300685906 Izabela Blitsman 308278514/d/C21_Ex01_3/AdvanceSandMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C21_Ex01_2;
+
+namespace C21_Ex01_3
+{
+    public class AdvanceSandMachine
+    {
+        private static int numberOfLines;
+        public static void ChooseNumberOfSandMachineHeight()
+        {
+            String readInputFromTheUser = "";
+            bool flagOfCorrectInputFromTheUser = false;
+
+            while (flagOfCorrectInputFromTheUser == false){
+                Console.WriteLine("Please enter the height of the Sand Machine or -1 to exit");
+                readInputFromTheUser = Console.ReadLine();
+
+                if (int.TryParse(readInputFromTheUser, out numberOfLines))
+                {
+                    if (numberOfLines > 0 || numberOfLines == -1)
+                    {
+                        flagOfCorrectInputFromTheUser = true;
+                        numberOfLines = numberOfLines % 2 == 0 ? numberOfLines + 1 : numberOfLines;
+                        C21_Ex01_2.BeginnersSandMachine.PrintSandMachine(C21_Ex01_2.BeginnersSandMachine.CreateSandMachine(new StringBuilder(), 0, numberOfLines));
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ChooseNumberOfSandMachineHeight();
+            Console.ReadLine();
+        }
+
+    }
+}
