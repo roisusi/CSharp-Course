daf05bfd28bb2deeb678deeb9c2ae29d605fefff
roisu <roisusi@gmail.com>
2021-08-03 21:32:23 +0300

    change GetColumnPlayerInput and all player coins to char

--- C21 Ex02 RoiSusi 300685906 Izabela Blitsman 308278514/C21 Ex02/C21 Ex02/MatrixCliUi.cs
+++ C21 Ex02 RoiSusi 300685906 Izabela Blitsman 308278514/C21 Ex02/C21 Ex02/MatrixCliUi.cs
@@ -10,16 +10,16 @@ namespace C21_Ex02
         private int m_width = 0;
         private int m_height = 0;
         private string [,] m_maxrixBorad;
-        private string [,] m_playerInput;
-        private List<string> m_currentHeightGame;
+        private char [,] m_playerInput;
+        private List<char> m_currentHeightGame;
 
         public MatrixCliUi(int width, int height)
         {
             this.m_width = width;
             this.m_height = height;
             m_maxrixBorad = new string[this.m_height, this.m_width];
-            m_playerInput = new string[this.m_height, this.m_width];
-            m_currentHeightGame = new List<string>(this.m_height);
+            m_playerInput = new char[this.m_height, this.m_width];
+            m_currentHeightGame = new List<char>(this.m_height);
         }
 
         public void InitiateNewGame()
@@ -28,7 +28,7 @@ namespace C21_Ex02
             {
                 for (int j = 0; j < m_playerInput.GetLength(1); j++)
                 {
-                    m_playerInput[i, j] = " ";
+                    m_playerInput[i, j] = ' ';
                 }
             }
         }
@@ -64,21 +64,24 @@ namespace C21_Ex02
             return m_maxrixBorad;
         }
 
-        public void AddCoin(int i_width, int i_height, string i_coin)
+        public void AddCoin(int i_width, int i_height, char i_coin)
         {
             m_playerInput[i_width, i_height] = i_coin;
         }
 
-        public string GetIndex(int i_width, int i_height)
+        public char GetIndex(int i_width, int i_height)
         {
             return m_playerInput[i_width, i_height];
         }
 
-        public List<string> GetColumnPlayerInput(int i_height)
+        public List<char> GetColumnPlayerInput(int i_height)
         {
             for (int width = 0; width < m_playerInput.GetLength(0); width++)
             {
-                m_currentHeightGame.Add(m_playerInput[width, i_height]);
+                if (!m_playerInput[width, i_height].Equals('\0'))
+                {
+                    m_currentHeightGame.Add(m_playerInput[width, i_height]);
+                }
             }
             return m_currentHeightGame;
         }
