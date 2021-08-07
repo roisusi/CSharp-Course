676a36fe9eda9e1c2f40392fcfe2297bee5df59b
roisu <roisusi@gmail.com>
2021-08-07 23:32:00 +0300

    added Enum GarageStatus

--- C21 Ex03 RoiSusi 300685906 Izabela Blitsman 308278514/Project1/Project1/GarageStatus.cs
+++ C21 Ex03 RoiSusi 300685906 Izabela Blitsman 308278514/Project1/Project1/GarageStatus.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Project1
+﻿enum GarageStatus
 {
-    class GarageStatus
-    {
-    }
+    InRepair,
+    Fixed,
+    Paid
 }
