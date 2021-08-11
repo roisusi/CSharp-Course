5fee38ade9b4fcd8c73e4aac10f01196e91cad7e
IzabelaB205 <izabelablitsman@gmail.com>
2021-08-11 10:48:52 +0300

    added ValueOutOfRangeExceptoinc lass

---
+++ C21 Ex03 RoiSusi 300685906 Izabela Blitsman 308278514/Project1/Project1/ValueOutOfRangeException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Project1
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        private float m_MaxValue;
+
+        public float MaxValue
+        {
+            get { return m_MaxValue; }
+        }
+
+        private float m_MinValue;
+
+        public float MinValue
+        {
+            get { return m_MinValue; }
+        }
+
+        public ValueOutOfRangeException(
+            Exception i_InnerException,
+            float i_MaxValue,
+            float i_MinValue)
+            : base(string.Format("An value out of range error occured - the range should be bettween {0} and {1}", i_MinValue, i_MaxValue),
+                  i_InnerException)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+
+        }
+    }
+}
