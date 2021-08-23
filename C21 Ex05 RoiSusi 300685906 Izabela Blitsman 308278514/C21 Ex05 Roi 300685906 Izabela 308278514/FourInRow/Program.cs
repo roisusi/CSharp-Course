a892a03ec9973c30c2094933146eb7f1792a9cd3
IzabelaB205 <izabelablitsman@gmail.com>
2021-08-23 14:07:04 +0300

    unfinished - game settings UI

---
+++ C21 Ex05 RoiSusi 300685906 Izabela Blitsman 308278514/C21 Ex05 Roi 300685906 Izabela 308278514/FourInRow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FourInRow
+{
+    static class Program
+    {
+        static void Main()
+        {
+            GameSettings gameSettings = new GameSettings();
+            gameSettings.ShowDialog();
+        }
+    }
+}
